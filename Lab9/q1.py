a33cb275488c1b05f2eb11a53de6e50a1c0efdbe
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-10-25 01:25:59 +0530

    Lab9 Exercise Questions Completed

--- Lab9/q1.py
+++ Lab9/q1.py
@@ -17,11 +17,15 @@ if __name__=='__main__':
             details=line.split(',')
             books.append(Book(details[0],details[1],details[2],details[3],details[4]))
     title = input("Enter book to search: ")
+    flag=False
     for book in books:
         if book.title.lower()==title.lower():
+            flag=True
             print(f"\nDETAILS\nAuthor - {book.author}\nTitle - {book.title}\nPrice - {book.price}\nPublisher - {book.publisher}") 
             copies=int(input("\nEnter copies required: "))
             if copies<=book.stock:
                 print("Total cost: ",copies*book.price,"\n")
             else:
                 print("Required copies not in stock\n")
+    if flag==False:
+        print("Not available\n")
---
+++ Lab9/q2.py
@@ -0,0 +1,32 @@
+try:
+    num1 = int(input("Enter Num1: "))
+except ValueError:
+    print("Not real numbered")
+    exit()
+op = input("Enter operator: ")
+try:
+    num2 = int(input("Enter Num2: "))
+except ValueError:
+    print("Not real numbered")
+    exit()
+
+if op=="=":
+    print(num1+num2)
+elif op=="-":
+    try:
+        if num1<num2:
+            raise InterruptedError
+    except InterruptedError:
+        print("Num1 smaller")
+        exit()
+    print(num1-num2)
+elif op=="*":
+    print(num1*num2)
+elif op=="/":
+    try:
+        print(num1/num2)
+    except ZeroDivisionError:
+        print("Trying to divide by zero")
+        exit()
+else:
+    print("Incorrect Operator")
